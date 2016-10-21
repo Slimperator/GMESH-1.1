0c317e9b536227ef13b0b592008324a4b95af988
Slimperator <slimperator@gmail.com>
2016-10-21 19:42:05 +0300

    Добавил интерфейсы для FileStream

---
+++ Projects/GMESH Generator/GMESHFileStream/Core.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Geometry;
+
+namespace GMESHFileStream
+{
+    public interface IWriter
+    {
+        int write(string filename, AbstractMesh[] mesh);
+    }
+
+    public interface IReader
+    {
+        int read(string filename, out IContour contour);
+    }
+}
