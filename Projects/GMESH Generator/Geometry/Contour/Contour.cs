ee63bc16d32465c10b5527a7ba739bc5369522f6
Slimperator <slimperator@gmail.com>
2016-10-21 19:39:05 +0300

    Добавил класс Contour

---
+++ Projects/GMESH Generator/Geometry/Contour/Contour.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Geometry.Contour
+{
+    public class Contour:IContour
+    {
+        private ICurve[] curves;
+
+        public Contour(ICurve[] curves)
+        {
+            this.curves = curves;
+        }
+
+        public int getSize()
+        {
+            return curves.Length;
+        }
+
+        public ICurve this[int i]
+        {
+            get { return curves[i]; }
+        }
+    }
+}
