b27c32377330dadf9c04443c45a7bf2248982384
Slimperator <slimperator@gmail.com>
2016-10-21 19:39:52 +0300

    Добавил реализацию класса RegMesh2D

---
+++ Projects/GMESH Generator/Geometry/Mesh/RegMesh2D.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Geometry
+{
+    public class RegMesh2D: AbstractMesh
+    {
+        public RegMesh2D(int size, int sigma)
+        {
+            this.meshPoints = new IPoint[size, size];
+            this.sigma = sigma;
+        }
+    }
+}
