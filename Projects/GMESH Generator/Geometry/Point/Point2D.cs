826fb25a53a66bec17a9d8ca46c319036220b628
Slimperator <slimperator@gmail.com>
2016-10-21 19:40:15 +0300

    Добавил реализацию класса точки2D

---
+++ Projects/GMESH Generator/Geometry/Point/Point2D.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Geometry.Point
+{
+    public class Point2D:IPoint
+    {
+        public double x { get; set; }
+        public double y { get; set; }
+        public Point2D(double x, double y)
+        {
+            this.x = x;
+            this.y = y;
+        }
+    }
+}
