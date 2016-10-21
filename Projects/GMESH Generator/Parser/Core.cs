8f0c7631632bf3364b71359abf3be17e1a48d7dd
Slimperator <slimperator@gmail.com>
2016-10-21 19:42:19 +0300

    Добавил интерфейсы для парсера

---
+++ Projects/GMESH Generator/Parser/Core.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Geometry;
+using GMESHFileStream;
+
+namespace Parser
+{
+    public interface IPreprocessing
+    {
+        void convert(IReader reader, out IContour contour);
+    }
+
+    public interface IPostprocessing
+    {
+        void convert(IWriter writer, AbstractMesh[] meshs);
+    }
+}
