79af233f24add6e8409358f6b867007deee47df4
Slimperator <slimperator@gmail.com>
2016-10-23 16:41:26 +0300

    Feature Добавил класс-заглушку Generator
    Тестовый вариант.

---
+++ Projects/GMESH Generator/Generator/Generator.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Geometry;
+
+namespace Generator
+{
+    public class Generator:IMeshGenerator
+    {
+        private IContour contour;
+        private int meshSize;
+        private IPoint[] aSideCutPoints, bSideCutPoints;
+        public Generator()
+        { 
+        }
+        public AbstractMesh generate(IContour contour)
+        {
+            this.contour = contour;
+            meshSize = contour[0].cutPoints.Length;
+            return null;
+        }
+    }
+}
