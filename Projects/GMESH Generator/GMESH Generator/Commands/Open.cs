cc6cdc9e96add39ae75ec96d3f5526c7ca9bb1ee
Slimperator <slimperator@gmail.com>
2016-10-25 18:13:12 +0300

    Add Класс комманды Open с заглушкой

---
+++ Projects/GMESH Generator/GMESH Generator/Commands/Open.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace GMESH_Generator.Commands
+{
+    public class Open: ICommand
+    {
+        public void callBack()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
