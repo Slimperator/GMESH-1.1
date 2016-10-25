71f19a6c3b063f9570d60ed94479738dcf224ee2
Slimperator <slimperator@gmail.com>
2016-10-25 19:54:22 +0300

    Fix Убрана рекурсия из get,set. Добавлен конструктор
    Теперь сетка не только квадрат.

--- Projects/GMESH Generator/Geometry/Mesh/RegMesh2D.cs
+++ Projects/GMESH Generator/Geometry/Mesh/RegMesh2D.cs
@@ -8,24 +8,31 @@ namespace Geometry
     public class RegMesh2D: AbstractMesh
     {
         private double Sigma;
-        private int size;
+        private int x,y;
         public RegMesh2D(int size, int sigma)
         {
             this.meshPoints = new IPoint[size, size];
-            this.size = size;
+            this.x = size;
+            this.y = size;
             this.sigma = sigma;
         }
-
+        public RegMesh2D(int rows, int columns, int sigma)
+        {
+            this.x = columns;
+            this.y = rows;
+            this.sigma = sigma;
+            this.meshPoints = new IPoint[y, x];
+        }
         public override int rows
         {
-            get { return this.size; }
-            protected set { this.size = value; }
+            get { return this.y; }
+            protected set { this.y = value; }
         }
 
         public override int colums
         {
-            get { return this.size; }
-            protected set { this.size = value; }
+            get { return this.x; }
+            protected set { this.x = value; }
         }
 
         public override double sigma
