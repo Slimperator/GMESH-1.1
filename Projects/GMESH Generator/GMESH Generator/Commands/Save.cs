762bc076075fe81467c9ca04f3b64db28297c8d5
Slimperator <slimperator@gmail.com>
2016-10-25 18:13:22 +0300

    Add Класс комманды Save с заглушкой

---
+++ Projects/GMESH Generator/GMESH Generator/Commands/Save.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace GMESH_Generator.Commands
+{
+    public class Save: ICommand
+    {
+        public void callBack()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
