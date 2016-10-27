e5709d3911be4cc6d14a55c7152e40eebcd4331f
Slimperator <slimperator@gmail.com>
2016-10-27 20:06:03 +0300

    Feature Комманда сохранения файла
    С комментариями

--- Projects/GMESH Generator/GMESH Generator/Commands/Save.cs
+++ Projects/GMESH Generator/GMESH Generator/Commands/Save.cs
@@ -2,14 +2,24 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using GMESHFileStream;
 
 namespace GMESH_Generator.Commands
 {
     public class Save: ICommand
     {
+        private Buffer storage;         //тут у нас пути сохранения, сетки, контура. буфер, в общем.
+        private GMESHFileStream.IWriter writer = new GMESHFileStream.OBJFile.OBJCreator();            //создаем экземпляр писалки
+        public Save()                   
+        {
+            storage = Buffer.getInstance(); //в конструкторе берем ссылку на наш буфер
+        }
         public void callBack()
         {
-            throw new NotImplementedException();
+            if (storage.Meshs == null || storage.PathSave == null)   //проверяем, есть ли что сохранять, и есть ли куда сохранять
+                return;
+
+            writer.write(storage.PathSave,storage.Meshs);             //сохраняем
         }
     }
 }
