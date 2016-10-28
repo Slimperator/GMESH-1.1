95ab9863fc4b7c31b29d3618823ab894f603ebe6
Daria <gorschkova15daria@yandex.ru>
2016-10-28 15:46:43 +0300

    Добавила класс Безье и команду GradAnalize

--- Projects/GMESH Generator/GMESH Generator/Commands/GradAnalize.cs
+++ Projects/GMESH Generator/GMESH Generator/Commands/GradAnalize.cs
@@ -2,14 +2,34 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using Analyzer.Grade;
+using Analyzer;
 
 namespace GMESH_Generator.Commands
 {
     public class GradAnalize: ICommand
     {
+        private Buffer storage;
+        private IGrade grade = new ArithmMeanGrade();
+        private GMESHFileStream.IWriter writer = new GMESHFileStream.OBJFile.OBJCreator();
+
+        private double estimate;
+        public double Estimate
+        {
+            get { return estimate; }
+        }
+
+        public GradAnalize()
+        {
+            storage = Buffer.getInstance();
+        }
+
         public void callBack()
         {
-            throw new NotImplementedException();
+            if (storage.Meshs == null || storage.PathSave == null)   //проверяем, есть ли что сохранять, и есть ли куда сохранять
+                return;
+
+            estimate = grade.calculate(storage.Meshs[0]);
         }
     }
 }
---
+++ Projects/GMESH Generator/Geometry/Curve/Bezier.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace Geometry.Curve
+{
+    public class Bezier : ICurve
+    {
+        public IPoint P0 { get; private set; }
+        public IPoint P1 { get; private set; }
+        public IPoint P2 { get; private set; }
+        public IPoint P3 { get; private set; }
+
+        public Bezier(IPoint P0, IPoint P1, IPoint P2, IPoint P3)
+        {
+            this.P0 = P0;
+            this.P1 = P1;
+            this.P2 = P2;
+            this.P3 = P3;
+        }
+
+        public void getPoint(double t, out double x, out double y)
+        {
+
+            x = this.P0.x * (1 - t) * (1 - t) * (1 - t) + 3 * t * P1.x * (1 - t) * (1 - t) + 3 * t * t * P2.x * (1 - t) + t * t * t * P3.x;
+            y = this.P0.y * (1 - t) * (1 - t) * (1 - t) + 3 * t * P1.y * (1 - t) * (1 - t) + 3 * t * t * P2.y * (1 - t) + t * t * t * P3.y;
+        }
+
+        //public void accept(IVisitor visitor)
+        //{
+        //    visitor.visitBezier(this);
+        //}
+
+        public IPoint[] cutPoints
+        {
+            get
+            {
+                throw new NotImplementedException();
+            }
+            set
+            {
+                throw new NotImplementedException();
+            }
+        }
+
+        public IPoint getPoint(double t)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
+
