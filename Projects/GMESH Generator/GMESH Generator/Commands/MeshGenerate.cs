362fb4f6172c9dc19401b695b539682d09877ef1
Slimperator <slimperator@gmail.com>
2016-10-31 16:25:36 +0300

    Fix Неправильное определение длины контура; Add Функция автоматического разбиения контура

--- Projects/GMESH Generator/GMESH Generator/Commands/MeshGenerate.cs
+++ Projects/GMESH Generator/GMESH Generator/Commands/MeshGenerate.cs
@@ -23,30 +23,33 @@ namespace GMESH_Generator.Commands
         {
             if (storage.Contour == null) 
                 return;
-            switch (storage.Contour.Length)                //определяем необходимость декомпозиции контура
+            switch (storage.Contour[0].getSize())                //определяем необходимость декомпозиции контура
             {
                 case 3:
+                    decomposeContourLine(storage.Contour[0]);
                     decomposer = new Decompose.Triangle.TriangleDecompose();
                     storage.Contour = decomposer.decompose(storage.Contour[0]);
                     break;
                 case 5:
+                    decomposeContourLine(storage.Contour[0]);
                     decomposer = new Decompose.Pentagon.PentagonDecTetraAndTri();
                     storage.Contour = decomposer.decompose(storage.Contour[0]);
                     break;
             }
             foreach (IContour x in storage.Contour)       //для всех контуров генерируем сетку
             {
+                decomposeContourLine(x);
                 meshs.Add(generetor.generate(x));
             }
             storage.Meshs = meshs.ToArray();
         }
-        /*private void decomposeContourLine(IContour contour)
+        private void decomposeContourLine(IContour contour)
         {
-            for (int i = 0; i < 4; i++)
+            for (int i = 0; i < contour.getSize(); i++)
             {
                 if (contour[i].cutPoints == null)
-                    Geometry.Curve.Tools.slittingCurve()
+                    Geometry.Curve.Tools.slittingCurve(contour.lenghtOfPart, contour[i]);
             }
-        }*/
+        }
     }
 }
