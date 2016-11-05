9b46973f6d46c092a160ab00764d245ac26c12f5
Slimperator <slimperator@gmail.com>
2016-11-06 00:15:08 +0300

    Update slittingCurve(double,ICurve)
    Теперь обладает "умным" алгоритмом разрезки.
    Деформирует линию таким образом, чтобы избавиться от "хвоста", когда не
    хватило длины для полноценной точки.
    Внедряет погрешность в генерацию регулярной сетки.

--- Projects/GMESH Generator/Geometry/Curve/Tools.cs
+++ Projects/GMESH Generator/Geometry/Curve/Tools.cs
@@ -13,7 +13,7 @@ namespace Geometry.Curve
         /// </summary>
         /// <param name="curve">Кривая, длина которой измеряется данным методом</param>
         /// <returns>Длина кривой</returns>
-        public static double length(ICurve curve)
+        public static double length(ICurve curve) //ХРАНИТЬ В КРИВОЙ, ВМЕСТО ПОСТОЯННЫХ ВЫЗОВОВ. Оптимизация
         {
             IPoint A, B;
             double result = 0.0;
@@ -41,13 +41,21 @@ namespace Geometry.Curve
             List<IPoint> cutPoints = new List<IPoint>();
             List<double> param = new List<double>();
             double d = Tools.length(curve);
-            for (double i = 0; i < d; i += length)             //добавить =
+            double i = 0.0;
+            for (; i <= d; i += length)             //добавить =
             {
                 param.Add(getParam(curve, i));
-                cutPoints.Add(curve.getPoint(getParam(curve,i)));
+                cutPoints.Add(curve.getPoint(getParam(curve, i)));
             }
-            curve.cutPoints = cutPoints.ToArray();
-            curve.cutParams = param.ToArray();
+            if (d - i == 0)                                 //если остатка у линии нет, то возвращаем найденные точки    
+            {
+                curve.cutPoints = cutPoints.ToArray();
+                curve.cutParams = param.ToArray();
+            }
+            if (d - i >= length / 2)                         //если остаток больше половины длины ячейки, то делаем длину меньше, количество точек увеличиваем на 1
+                slittingCurve(cutPoints.Count() + 1, curve);
+            else
+                slittingCurve(cutPoints.Count(), curve);     //если остаток меньше половины длины ячейки, то делаем длину больше, количество точек не меняется
         }
         public static void slittingCurve(int points, ICurve curve)
         {
@@ -57,8 +65,8 @@ namespace Geometry.Curve
             double d = Tools.length(curve);
             for (double i = 0; i <= 1; i += tOfPart)              //добавить =
             {
-                    param.Add(i);
-                    cutPoints.Add(curve.getPoint(i));
+                param.Add(i);
+                cutPoints.Add(curve.getPoint(i));
             }
             if (cutPoints.Count != points)
             {
@@ -74,7 +82,7 @@ namespace Geometry.Curve
         public static bool equalityCurves(ICurve curve1, ICurve curve2)
         {
             bool straight = true, forward = true;
-            if(curve1.cutPoints.Length != curve2.cutPoints.Length)
+            if (curve1.cutPoints.Length != curve2.cutPoints.Length)
             {
                 return false;
             }
