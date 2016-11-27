5fea876fc80cf0c1ddc7b27bf812384fbeac667b
Ani <kirakosyan.ani@mail.ru>
2016-11-27 13:44:52 +0300

    fix bugs
    убрала лишние аргументы

--- Projects/GMESH Generator/GMESH Generator/Commands/Save.cs
+++ Projects/GMESH Generator/GMESH Generator/Commands/Save.cs
@@ -19,7 +19,7 @@ namespace GMESH_Generator.Commands
             if (storage.Meshs == null || storage.PathSave == null)   //проверяем, есть ли что сохранять, и есть ли куда сохранять
                 return;
             Console.WriteLine("Пишем сетку...");
-            writer.write(storage.PathSave,storage.Meshs, storage.Contour);             //сохраняем
+            writer.write(storage.PathSave,storage.Meshs);             //сохраняем
         }
     }
 }
