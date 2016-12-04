a0eaa4e33af0cd53f09957a4416e0ec19c6ed46b
Ani <kirakosyan.ani@mail.ru>
2016-12-04 22:06:40 +0300

    add obj reader

---
+++ Projects/GMESH Visualizator/GMESH Visualizer/Parcer/ObjReader.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Geometry;
+using Geometry.Point;
+using System.IO;
+
+namespace Parcer
+{
+    public class ObjReader
+    {
+        public List<IPoint> objReader(string path)
+        {
+            List<IPoint> listPoint = new List<IPoint>();//сюдабудем писать вершинки
+            StreamReader objReader = new StreamReader(path);
+            string[] objPoints;
+            string[] objLines;
+            int[] objPointsID = new int[3];//значение х у для каждой точки
+            List<int> forPows = new List<int>();
+            IPoint tmp = null;
+            char delimiter = ' ';
+            string line;
+            //String[] substrings = value.Split(delimiter);
+
+            while ((line = objReader.ReadLine()) != null)
+            {
+                if (line.ToString() != "")
+                {
+                    if (line[0].ToString() == "v")
+                    {
+                        objPoints = line.Split(delimiter);
+                        listPoint.Add(new Point2D(Convert.ToDouble(objPoints[1]), Convert.ToDouble(objPoints[2])));
+
+                    }
+                    // else break;
+                    if (line[0].ToString() == "l")
+                    {
+                        objLines = line.Split(delimiter);
+                        forPows.Add(Convert.ToInt32(objLines[1]));
+                        forPows.Add(Convert.ToInt32(objLines[2]));
+                    }
+
+
+                }
+                var arr = forPows.GroupBy(j =>j).Select(j => j.Count()).ToArray();
+                for (int i = 1; i < listPoint.Count; i++ )
+                {
+                    listPoint[i].rate = arr[i];
+            }
+            }
+            return listPoint;
+        }
+
+    }
+}
