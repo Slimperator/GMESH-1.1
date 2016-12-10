858af31438fbfdaa982b62ce668fc741004b03c2
Slimperator <slimperator@gmail.com>
2016-12-10 13:50:36 +0300

    Add Добавлен класс Буфера
    Пока недоделанный

---
+++ Projects/GMESH Visualizator/GMESH Visualizer/GMESH Visualizer/Buffer.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace GMESH_Visualizer
+{
+    public class Buffer
+    {
+        static Buffer instance = null;
+        static private Buffer()
+        { }
+        static Buffer getInstance()
+        {
+            if (instance == null)
+                instance = new Buffer();
+            return instance;
+        } 
+    }
+}
