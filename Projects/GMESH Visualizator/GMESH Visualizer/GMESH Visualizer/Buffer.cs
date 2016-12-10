26a1716cbfc04b34699e0c38e78dafda5a485e37
Slimperator <slimperator@gmail.com>
2016-12-10 16:08:04 +0300

    Update Buffer

--- Projects/GMESH Visualizator/GMESH Visualizer/GMESH Visualizer/Buffer.cs
+++ Projects/GMESH Visualizator/GMESH Visualizer/GMESH Visualizer/Buffer.cs
@@ -2,15 +2,24 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using Geometry;
+using Preprocessing;
+using Errors;
 
 namespace GMESH_Visualizer
 {
     public class Buffer
     {
         static Buffer instance = null;
+        List<IPoint> points { get; set; }
+        List<ICurve> lines { get; set; }
+        List<IError> errors { get; set; }
+        List<IContour> contour { get; set; }
+        graph graph { get; set; }
+
         static private Buffer()
         { }
-        static Buffer getInstance()
+        static public Buffer getInstance()
         {
             if (instance == null)
                 instance = new Buffer();
