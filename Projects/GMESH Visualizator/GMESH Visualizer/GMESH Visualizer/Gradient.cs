2286c6ee17aa201fc5e2f468f544c9626ad505a1
alexklimov <klimwork93@mail.ru>
2016-12-11 22:00:17 +0300

    add function in Gradient.cs
    for quadrangle from 0 to -1 return Gray Color

--- Projects/GMESH Visualizator/GMESH Visualizer/GMESH Visualizer/Gradient.cs
+++ Projects/GMESH Visualizator/GMESH Visualizer/GMESH Visualizer/Gradient.cs
@@ -6,26 +6,40 @@ using System.Drawing;
 
 namespace GMESH_Visualizer
 {
-   public class Gradient
+    public class Gradient
     {
-      // В данном методе возвращется цвет. Чем ближе будет оценка (status) к 1, тем более красным будет цвет
-      // status= 0.01 - зелёный
-      // status= 1 - ярко-красный
-         double status; // оценка квадратика          
+        // В данном методе возвращется цвет. Чем ближе будет оценка (status) к 1, тем более красным будет цвет
+        // status= 0.01 - зелёный
+        // status= 1 - ярко-красный
+        double status; // оценка квадратика          
 
-       public Color GetCellColor(double status)
-       {
-           if (status > 1) status = 1;
-           if (status < 0) status = 0;
+        public Color GetCellColor(double status)
+        {
+            if (status > 1) status = 1;
+            if (status < 0) status = 0;
 
-           int G = (int)(255.0 * status);
-           int R = 255 - G;
-           int B = 0;
-           int A = 255;
+            int G = (int)(255.0 * status);
+            int R = 255 - G;
+            int B = 0;
+            int A = 255;
 
-           return Color.FromArgb(A, R, G, B);
-       }
-       
+            return Color.FromArgb(A, R, G, B);
+        }
+        // для значений от 0 до -1 вернется серый цвет, обратить внимание что разные функции !!!
+        public Color GetGrayColor(double status)
+        {
+
+            if (status > -1) status = -1;
+            if (status < 0) status = 0;
+
+            //int G = (int)(255.0 * status);
+            //int R = 255 - G;
+            //int B = 0;
+            //int A = 255;
+
+            return Color.Gray;
+
+        }
     }
 }
 
