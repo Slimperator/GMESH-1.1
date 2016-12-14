3a6bfb6764f2ea874428e20ddd099e773129dca5
Slimperator <slimperator@gmail.com>
2016-12-14 17:58:44 +0300

    Add Интерфейс IReader

--- Projects/GMESH Visualizator/GMESH Visualizer/Parcer/Core.cs
+++ Projects/GMESH Visualizator/GMESH Visualizer/Parcer/Core.cs
@@ -2,10 +2,12 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using Geometry;
 
 namespace Parcer
 {
-    public class Core
+    public interface IReader
     {
+        void read(string path, out List<IPoint> listPoint, out List<ICurve> listCurve, out Preprocessing.graph.edge[][] meshCells);
     }
 }
