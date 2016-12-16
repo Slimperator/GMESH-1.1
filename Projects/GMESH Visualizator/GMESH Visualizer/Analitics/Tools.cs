2698e556850c8dd534ba9d90446e1079dec0fe34
Slimperator <slimperator@gmail.com>
2016-12-16 20:26:09 +0300

    Add Tools class
    -добавлен прототип под функцию проверки пересечения кривых

---
+++ Projects/GMESH Visualizator/GMESH Visualizer/Analitics/Tools.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Geometry;
+
+namespace Analitics
+{
+    public class Tools
+    {
+        public static bool lineIntersectionCheck(IPoint a1, IPoint b1, IPoint a2, IPoint b2)
+        {
+            return false;
+        }
+    }
+}
