ce66bbe0b352f6cc794ccd6fa2ed947386077a68
AniKirakosyan <kirakosyan.ani@mail.ru>
2016-12-19 20:53:18 +0300

    Заполнила футкцию пересечения отрезков

--- Projects/GMESH Visualizator/GMESH Visualizer/Analitics/Tools.cs
+++ Projects/GMESH Visualizator/GMESH Visualizer/Analitics/Tools.cs
@@ -10,7 +10,19 @@ namespace Analitics
     {
         public static bool lineIntersectionCheck(IPoint a1, IPoint b1, IPoint a2, IPoint b2)
         {
-            return false;
+            if (((a1.y - b1.y) * (b2.x - a2.x) - (a2.y - b2.y) * (b1.x - a1.x)) == 0 || (b2.x - a2.x) == 0)
+                return false;
+            else
+            {
+                double x = ((a1.x * b1.y - b1.x * a1.y) * (b2.x - a2.x) - (a2.x * b2.y - b2.x * a2.y) * (b1.x - a1.x)) / ((a1.y - b1.y) * (b2.x - a2.x) - (a2.y - b2.y) * (b1.x - a1.x));
+                double y = ((a2.y - b2.y) * x - (a2.x * b2.y - b2.x * a2.y)) / (b2.x - a2.x);
+
+                if (((a1.x <= x) && (b1.x >= x) && (a2.x <= x) && (b2.x >= x)) || ((a1.y <= y) && (b1.y >= y) && (a2.y <= y) && (b2.y >= y)))
+
+                    return true;
+                else
+                    return false;
+            }
         }
     }
 }
